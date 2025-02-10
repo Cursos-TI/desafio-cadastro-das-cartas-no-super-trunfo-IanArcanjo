9538ffa2baea3c9e1646f92d85b4f3d05f9b95e0
IanArcanjo <iandc11103@gmail.com>
2025-02-10 20:05:00 +0000

    Declaração de variáveis e apresentação do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,18 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char codigo; // Código da carta
+    char nome[50]; // Nome da Cidade
+    int populacao; // População da cidade
+    float area; // Área da cidade
+    float pib; // PIB da Cidade
+    int turistico; // Número de pontos turísticos
+
+    printf ("SUPER TRUNFO PAÍSES\n");
+    printf ("Seja bem-vindo(a) para o jogo SUPER TRUNFO PAÍSES. Nesse jogo você deverá cadastrar cartas de cidadeas, informando dados da cidade à sua escolha,
+    como seu nome, sua população, área e algumas outras informações. "); // Aqui é a apresentação do jogo, para orientar o usuário o que deve ser feito.
+    
+    printf ("Informe o código da carta.")
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
